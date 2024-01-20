f9d48636b4802c322f3545e66abc17cc93671194
YuliaLevechko <125822613+YuliaLevechko@users.noreply.github.com>
2024-01-20 23:41:23 +0200

    refactoring code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import Section from './Section/Section';
+
+const  App =() => {
+    return (
+      <>
+        <Section title="Statistics" />
+      </>
+    )
+}
+
+export default App;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,61 @@
+import Statistics from 'components/Statistics/Statistics';
+import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
+import Notification from 'components/Notification/Notification';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+
+const Section = ({title}) => {
+  
+const [good , setGood] = useState(0);
+const [ neutral, setNeutral] = useState(0);
+const [ bad , setBad] = useState(0);
+
+const  countTotalFeedback = () => {
+  return bad + good + neutral;
+};
+const  countPositiveFeedbackPercentage = () => {
+  return Math.round((good * 100) / countTotalFeedback());
+};
+function  onLeaveFeedback ( options)  {
+  if(options === 'good'){
+   setGood((prevState => prevState + 1))
+  }
+  if(options === 'neutral'){
+     setNeutral((prevState => prevState + 1))
+  }
+  if(options === 'bad'){
+      setBad((prevState => prevState + 1))
+  }
+ console.log(options);
+};
+return (
+  <>
+    <h1>Please leave feedback</h1>
+    <FeedbackOptions
+      options={['good', 'neutral', 'bad']}
+      onLeaveFeedback = {onLeaveFeedback}
+    />
+
+    <h2>{title}</h2>
+    {countTotalFeedback() === 0 ? (
+      <Notification message="There is no feedback" />
+    ) 
+    : (
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        total={countTotalFeedback}
+        positivePercentage={countPositiveFeedbackPercentage}
+      />
+    )
+    }
+  </>
+)
+}
+
+export default Section;
+
+Section.propTypes = {
+  title: PropTypes.string
+};
